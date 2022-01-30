a6724ea8f5a1c5b945b4cc2233870826392a233f
AliVik <alvikul1994@gmail.com>
2022-01-30 20:55:04 +0200

    переписывает searchbar на хуки

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,51 +1,46 @@
-import React, { Component } from "react";
-import { RiSearchEyeLine } from "react-icons/ri";
-import { Toaster, toast } from "react-hot-toast";
-import css from "./Searchbar.module.css";
-import PropTypes from "prop-types";
+import { useState } from 'react';
+import { RiSearchEyeLine } from 'react-icons/ri';
+import { Toaster, toast } from 'react-hot-toast';
+import css from './Searchbar.module.css';
+import PropTypes from 'prop-types';
 
-export default class Searchbar extends Component {
-  state = {
-    formInput: "",
+export default function Searchbar({ onFormSubmit }) {
+  const [formInput, setFormInput] = useState('');
+
+  const onInputChange = evt => {
+    setFormInput(evt.target.value);
   };
-  onInputChange = (evt) => {
-    this.setState({ formInput: evt.target.value });
-  };
-  onFormSubmit = (evt) => {
-    const { formInput } = this.state;
+  const onSearchFormSubmit = evt => {
     evt.preventDefault();
     const form = evt.currentTarget;
-    this.props.onFormSubmit(this.state);
-    this.setState({ formInput: "" });
-    if (formInput === "") {
-      toast.error("You haven`t written anything yet");
+    onFormSubmit(formInput);
+    setFormInput('');
+    if (formInput === '') {
+      toast.error('You haven`t written anything yet');
     }
     form.reset();
   };
-
-  render() {
-    return (
-      <header className={css.Header}>
-        <form onSubmit={this.onFormSubmit} className={css.SearchForm}>
-          <button type="submit" className={css.SearchButton}>
-            <RiSearchEyeLine
-              style={{ width: "100%", height: "100%", color: "#fff" }}
-            />
-          </button>
-
-          <input
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.onInputChange}
-            className={css.SearchInput}
+  return (
+    <header className={css.Header}>
+      <form onSubmit={onSearchFormSubmit} className={css.SearchForm}>
+        <button type="submit" className={css.SearchButton}>
+          <RiSearchEyeLine
+            style={{ width: '100%', height: '100%', color: '#fff' }}
           />
-        </form>
-        <Toaster />
-      </header>
-    );
-  }
+        </button>
+
+        <input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={onInputChange}
+          className={css.SearchInput}
+        />
+      </form>
+      <Toaster />
+    </header>
+  );
 }
 
 Searchbar.propTypes = {
