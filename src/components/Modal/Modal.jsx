0686968a6f4020864fb4d618d04263baf2d187d2
AliVik <alvikul1994@gmail.com>
2022-01-30 20:55:19 +0200

    переписывает модалку на хуки

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,33 @@
-import { Component } from "react";
-import { createPortal } from "react-dom";
-import PropTypes from "prop-types";
-import css from "./Modal.module.css";
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import css from './Modal.module.css';
 
-const modalRoot = document.querySelector("#modal-root");
+const modalRoot = document.querySelector('#modal-root');
 
-export default class ModalForm extends Component {
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleEscClick);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleEscClick);
-  }
-  handleEscClick = (evt) => {
-    if (evt.code === "Escape") {
-      this.props.onClose();
+export default function ModalForm({ children, onClose }) {
+  const handleEscClick = evt => {
+    if (evt.code === 'Escape') {
+      onClose();
     }
   };
-  handleBackdropClick = (evt) => {
+  const handleBackdropClick = evt => {
     if (evt.target === evt.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    return createPortal(
-      <div className={css.Overlay} onClick={this.handleBackdropClick}>
-        <div className={css.Modal}>{this.props.children}</div>
-      </div>,
-      modalRoot
-    );
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', handleEscClick);
+    return () => {
+      window.removeEventListener('keydown', handleEscClick);
+    };
+  });
+  return createPortal(
+    <div className={css.Overlay} onClick={handleBackdropClick}>
+      <div className={css.Modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
 }
 
 ModalForm.propTypes = {
